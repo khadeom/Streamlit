33813ec73600fe07673e4fd684962a8bc5c2a367
khadeom <63163047+khadeom@users.noreply.github.com>
2021-08-10 13:26:33 +0530

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -2,26 +2,47 @@ import streamlit as st
 import qrcode
 import cv2
 
-st.title("App by aditi but does it work")
+st.set_page_config(layout="wide")
 
-st.write(""" 
-	#demo QRcode scanner and decoder
-	""")
+st.markdown("""
+<style>
+.big-font {
+    font-size:20px !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
-user_input = st.text_input("Enter text"
-	, "Hey m aditi")
+
+st.title("App by aditi")
+
+st.markdown('<p class="big-font">Demo qrcode generator and Scanner</p>'
+	, unsafe_allow_html=True)
+
+# st.markdown('<p class="big-font">Enter text</p>'
+# 	, unsafe_allow_html=True)
+
+user_input = st.text_input('Enter text',"Enter text here")
 
 st.write("QRcode for entered text")
-img=qrcode.make(user_input)
-img.save("adie.jpg")
+but=True
+if st.button('View') and but==True:
+
+	img=qrcode.make(user_input)
+	img.save("adie.jpg")
 
 
 
 
-st.image('adie.jpg', channels="BGR")
+	st.image('adie.jpg', channels="BGR")
 
 
-a=cv2.QRCodeDetector()
-val,points,straight_qrcode=a.detectAndDecode(cv2.imread("adie.jpg"))
-#print(val)
-st.write("Decoded text from QRcode :",val)
+	a=cv2.QRCodeDetector()
+	val,points,straight_qrcode=a.detectAndDecode(cv2.imread("adie.jpg"))
+	#print(val)
+	st.write("Decoded text from QRcode :",val)
+
+if st.button("Close"):
+	but=False
+
+# st.markdown('<p class="big-font">Hello World !!</p>'
+# 	, unsafe_allow_html=True)
